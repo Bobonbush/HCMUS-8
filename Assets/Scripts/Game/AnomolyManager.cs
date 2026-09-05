c784a500b9ea00db72c5db1fea07d7ae785153bc
Bobonbush <tdadung23@apcs.fitus.edu.vn>
2026-09-05 10:23:06 +0700

    Balanced anomoly

--- Assets/Scripts/Game/AnomolyManager.cs
+++ Assets/Scripts/Game/AnomolyManager.cs
@@ -115,6 +115,7 @@ public class AnomolyManager : MonoBehaviour
         int size = anomolies.Count;
         int randomMize = Random.Range(0, size);
 
+
         
         if (anomolies[randomMize] is Anomoly anomoly)
         {
